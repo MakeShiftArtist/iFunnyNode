a09278683ac05ac87ba01c6e830e7906a8fc62ab
MakeShiftArtist <lrmcbride4@gmail.com>
2021-12-29 23:38:04 -0600

    Fixed imports (hopefully)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export * from "./objects/index";
+import Client from "./objects/Client.js";
+import FreshObject from "./objects/FreshObject.js";
+import Post from "./objects/Post.js";
+import ImagePost from "./objects/Image.js";
+import VideoPost from "./objects/Video.js";
+import User from "./objects/User.js";
+
+export default Client;
+
+export { User, Post, ImagePost, VideoPost, FreshObject };
