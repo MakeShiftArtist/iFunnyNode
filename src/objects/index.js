494757ed15ae223b038c391df8613ab2fa1082e6
MakeShiftArtist <lrmcbride4@gmail.com>
2021-12-29 23:40:57 -0600

    Deleted unnecessary file

--- src/objects/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Client } from "./Client";
-export { default as FreshObject } from "./FreshObject";
-export { default as Image } from "./Image";
-export { default as Video } from "./Video";
-export { default as Post } from "./Post";
-export { default as User } from "./User";
