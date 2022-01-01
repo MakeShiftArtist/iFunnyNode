9e300086552bd5fa53ee6acb5515f594c971eb15
MakeShiftArtist <lrmcbride4@gmail.com>
2022-01-01 13:31:31 -0600

    Fixed auth tests

--- test/objects/client/test_auth.js
+++ test/objects/client/test_auth.js
@@ -1,22 +1,62 @@
-let Client = require("../../../src/objects/Client.js");
+import Client from "../../../src/objects/Client.js";
 
-let { expect } = require("chai");
+import { assert, expect } from "chai";
 
 const EMAIL = process.env["IFUNNY_NODE_EMAIL"] ?? "";
 const PASSWORD = process.env["IFUNNY_NODE_PASSWORD"] ?? "";
 
-beforeEach(async function () {
-  if (EMAIL === "" || PASSWORD === "") {
-    this.skip();
-  }
+let client = new Client();
+if (EMAIL === "" || PASSWORD === "") {
+	this.skip();
+}
 
-  this.client = new Client();
-  await this.client.login(EMAIL, PASSWORD);
+try {
+	await client.login({
+		email: EMAIL,
+		password: PASSWORD,
+	});
+} catch (err) {
+	switch (err.message) {
+		case "too_many_user_auths":
+			break;
+		case "captcha_required":
+			break;
+		case "access_token not given":
+			break;
+		default:
+			client.authorized = false;
+	}
+}
+describe("client auth", async () => {
+	it("headers use basic token", async () => {
+		let new_client = new Client();
+
+		expect(new_client.headers["Authorization"]).to.match(
+			/Basic [a-zA-Z0-9=]{156}/
+		);
+	});
+	it("uses token passed into constructor", async () => {
+		let new_client = new Client({ token: "foobar" });
+		expect(new_client.headers["Authorization"]).to.equal("Bearer foobar");
+	});
+	beforeEach(async () => {
+		if (!client.authorized) this.skip();
+	});
+	it("update headers on login", async () => {
+		expect(client.headers["Authorization"]).to.match(/Bearer [a-z0-9]{64}/);
+	});
+	it("has valid headers", async () => {
+		if (!client) {
+			this.skip();
+		}
+		let headers = client.headers;
+		expect(headers["Ifunny-Project-Id"]).to.equal("iFunny");
+		expect(headers["accept"]).to.equal(
+			"application/json,image/jpeg,image/webp,video/mp4"
+		);
+		expect(headers["applicationstate"]).to.equal(1);
+		expect(headers["accept-language"]).to.equal("en-US");
+		expect(headers["accept-encoding"]).to.equal("gzip");
+		expect(typeof headers["User-Agent"]).to.equal("string");
+	});
 });
-
-describe(
-  "client auth",
-  it("gets properties", async function () {
-    // TODO
-  }),
-);
