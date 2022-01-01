cb7101defc0c929a87d47d1ac40cda159265a79f
MakeShiftArtist <lrmcbride4@gmail.com>
2022-01-01 12:46:51 -0600

    Fixed sleep method failing

--- test/utils/methods/test_sleep.js
+++ test/utils/methods/test_sleep.js
@@ -1,17 +1,14 @@
-const methods = require("../../../src/utils/methods.js");
+import { sleep } from "../../../src/utils/methods.js";
 
-const { assert, expect } = require("chai");
+import { expect } from "chai";
 
 describe(
-  "sleep",
-  it(
-    "sleeps",
-    async () => {
-      const stamp = Date.now();
-      await methods.sleep(1);
-      const after = Date.now();
+	"sleep",
+	it("sleeps", async () => {
+		const stamp = Date.now();
+		await sleep(1);
+		const after = Date.now();
 
-      expect(after - stamp).to.be.within(1000, 1007);
-    },
-  ),
+		expect(after - stamp).to.be.within(1000, 1020);
+	})
 );
