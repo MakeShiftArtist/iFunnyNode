891fe9bd1ff5b9891eb5075b889cd2429c91df5a
MakeShiftArtist <lrmcbride4@gmail.com>
2022-01-31 01:12:35 -0600

    Added methods and fixed last_seen_at

--- src/objects/ChatObjects/ChatUser.js
+++ src/objects/ChatObjects/ChatUser.js
@@ -27,6 +27,13 @@ export default class ChatUser extends User {
 		 * @type {Object}
 		 */
 		this.chat_payload = payload;
+
+		/**
+		 * Is the user still in the chat?
+		 * @private
+		 * @type {Boolean}
+		 */
+		this._in_chat = true;
 	}
 
 	/**
@@ -63,11 +70,51 @@ export default class ChatUser extends User {
 
 	/**
 	 * When the user was last seen online
-	 * @type {Date|null}
+	 * @type {Promise<Date|null>}
 	 */
 	get last_seen_at() {
-		let time = this.chat_payload?.last_seen_at;
-		return time ? new Date(time) : null;
+		return (async () => {
+			let time = this.chat_payload?.last_seen_at;
+			if (time) {
+				return new Date(time);
+			}
+			await this.fetch();
+			time = this.chat_payload?.last_seen_at;
+			if (time === 0) {
+				return new Date();
+			} else if (time) {
+				return new Date(time);
+			} else return null;
+		})();
+	}
+
+	/**
+	 * Is the user currently online of the chat?
+	 * @type {Promise<Boolean>}
+	 */
+	get online() {
+		return (async () => {
+			let diff = (await this.fresh.last_seen_at).getTime() - new Date().getTime();
+			console.log(diff);
+			return diff === 0;
+		})();
+	}
+
+	/**
+	 * Is the user a member of the chat?
+	 * @type {Promise<Boolean>}
+	 */
+	get in_chat() {
+		return (async () => {
+			if (!this._update) {
+				return this._in_chat;
+			}
+			for await (let member of await this.context.channel.members) {
+				if ((await member.id) === (await this.id)) {
+					this._in_chat = true;
+				}
+			}
+		})();
 	}
 
 	/**
@@ -76,5 +123,20 @@ export default class ChatUser extends User {
 	 */
 	kick() {
 		return this.context.channel.kick(this);
+	}
+
+	/**
+	 * Fetches the ChatUser from the current contexts channel
+	 * @returns {Promise<ChatUser>}
+	 */
+	async fetch() {
+		if (!this.in_chat && !this._update) return;
+		let user = await this.context.channel.fetch_by_id(await this.id);
+		if (user && user.chat_payload) {
+			this.chat_payload = user.chat_payload;
+			return user.fresh;
+		}
+		this._in_chat = false;
+		return this.fresh;
 	}
 }
