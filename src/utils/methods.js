c29bcd085231146d1fa1c4dcd0334a61592cc738
MakeShiftArtist <lrmcbride4@gmail.com>
2022-02-15 23:12:16 -0600

    Added paginator

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -1,9 +1,16 @@
+// @ts-check
+
 import crypto from "crypto";
+import url from "url";
+
+/**
+ * @typedef {import('../objects/Client.js').default} Client
+ */
 
 /**
  * Capitalizes the first character of a string and makes the rest lowercase
- * @param {String} string
- * @returns {String} The capitalized version of the string
+ * @param {string} string
+ * @returns {string} The capitalized version of the string
  * @example
  * capitalize("hElLO WoRld") // Hello world
  */
@@ -13,7 +20,7 @@ export function capitalize(string) {
 
 /**
  * Asynchronously waits for a specified amount of time (in seconds)
- * @param {Number} seconds Amount of seconds to sleep for
+ * @param {number} seconds Amount of seconds to sleep for
  * @returns {Promise<void>} A promise that is resolved when the sleep is complete
  */
 export function sleep(seconds) {
@@ -24,23 +31,67 @@ export function sleep(seconds) {
  * Creates a basic token for iFunny requests\
  * Basic auth tokens MUST be `156` characters long
  * Basic auth tokens *should* also be made using UUIDv4
- * @returns {String} The basic auth token
+ * @returns {string} The basic auth token
  */
 export function create_basic_token() {
 	let uuid = crypto.randomUUID().replace(/\-/g, "");
-	let hex = crypto
-		.createHash("sha256")
-		.update(uuid)
-		.digest("hex")
-		.toUpperCase();
+	let hex = crypto.createHash("sha256").update(uuid).digest("hex").toUpperCase();
 	let a = hex + "_MsOIJ39Q28:";
 	let b = hex + ":MsOIJ39Q28:PTDc3H8a)Vi=UYap";
 	let c = crypto.createHash("sha1").update(b).digest("hex");
 	return Buffer.from(a + c).toString("base64");
 }
 
+/**
+ * Base paginator for API calls
+ * @param {Client} client
+ * @param {Object} opts Additional args for the paginator
+ * @param {string} opts.url URL to request in the paginator
+ * @param {string} opts.key Key to request in the paginator
+ * @param {Object|url.URLSearchParams} [opts.params] Parameters for the api requests
+ * @yields {Promise<any>}
+ */
+export async function* paginator(client, opts) {
+	// Check required opts
+	if (!opts.url || typeof opts.url !== "string") {
+		throw new Error("No url provided");
+	}
+	if (!opts.key || typeof opts.key !== "string") {
+		throw new Error("opts.key must be a string");
+	}
+
+	let hasNext = false;
+
+	do {
+		// Make request
+		let { data } = await client.instance.request({
+			url: opts.url,
+			params: new url.URLSearchParams(opts.params ?? {}),
+		});
+
+		// Get iterable
+		let items = data?.data?.[opts.key]?.items;
+		if (!Array.isArray(items)) throw new Error(`Items not an array: ${items}`);
+
+		// Store next variable
+		hasNext = data?.data?.[opts.key]?.paging?.hasNext ?? false;
+		if (hasNext) {
+			opts.params.next = data.data[opts.key].paging.cursors.next;
+		}
+
+		for (let item of items) {
+			yield item;
+		}
+		/*
+		if (!hasNext) {
+			console.log(data.data);
+		}*/
+	} while (hasNext);
+}
+
 export default {
 	capitalize,
 	sleep,
 	create_basic_token,
+	paginator,
 };
