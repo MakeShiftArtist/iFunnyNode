4868235ad1b39808b4258e83942f410c42638068
MakeShiftArtist <lrmcbride4@gmail.com>
2022-02-15 23:11:20 -0600

    Updated exports

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 import Client from "./objects/Client.js";
+import Chats from "./objects/Chats.js";
 import User from "./objects/User.js";
+import Guest from "./objects/small/Guest.js";
 import Post from "./objects/Post.js";
-import ImagePost from "./objects/Image.js";
-import VideoPost from "./objects/Video.js";
-import ImagePost from "./objects/Image.js";
+import ImagePost from "./objects/small/ImagePost.js";
+import VideoPost from "./objects/small/VideoPost.js";
+import CaptionPost from "./objects/small/Captionpost.js";
 import Ban from "./objects/small/Ban.js";
 import FreshObject from "./objects/FreshObject.js";
 
 import Channel from "./objects/ChatObjects/Channel.js";
-import ChatFile from './objects/ChatObjects/ChatFile.js';
+import ChatFile from "./objects/ChatObjects/ChatFile.js";
 import ChatPaginator from "./objects/ChatObjects/ChatPaginator.js";
 import ChatUser from "./objects/ChatObjects/ChatUser.js";
 import Context from "./objects/ChatObjects/Context.js";
 import Message from "./objects/ChatObjects/Message.js";
 import Socket from "./objects/ChatObjects/SocketInstance.js";
 
-const ChatObjects = {
+export default Client;
+
+export {
+	Client,
+	Chats,
+	User,
+	Guest,
+	Post,
+	ImagePost,
+	VideoPost,
+	CaptionPost,
+	FreshObject,
+	Ban,
 	Channel,
 	ChatFile,
 	ChatPaginator,
 	ChatUser,
 	Context,
 	Message,
-	Socket
-}
-
-export default Client;
-
-export {Client as default, Client, User, Post, ImagePost, VideoPost, FreshObject, Ban, ChatObjects };
+	Socket,
+};
