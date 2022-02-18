9a90f3bfb77d1e0bb5c094c8cac08af55170ed15
MakeShiftArtist <lrmcbride4@gmail.com>
2022-02-18 00:56:13 -0600

    Added Comment.js

--- src/objects/Comment.js
+++ src/objects/Comment.js
@@ -8,6 +8,7 @@ import { get_post_type } from "../utils/methods.js";
  * @typedef {import("./Client.js").default} Client
  * @typedef {import('../objects/small/ImagePost.js').default} ImagePost
  * @typedef {import('../objects/small/VideoPost.js').default} VideoPost
+ * @typedef {import('../objects/small/Reply.js').default} Reply
  */
 
 /**
@@ -17,25 +18,47 @@ import { get_post_type } from "../utils/methods.js";
  */
 export default class Comment extends FreshObject {
 	/**
-	 * @param {string} id Id of the comment
+	 * @param {Object} comment The comment's raw object
 	 * @param {Client} client Client the comment object is associated with
-	 * @param {Object} [opts]
+	 * @param {Object} [opts] Optional args for the constructor
 	 * @param {string} [opts.url] Url to make requests to
+	 * @param {Object} [opts.data] Data to add to the payload
 	 */
-	constructor(id, client, opts = {}) {
-		super(id, client, opts);
+	constructor(comment, client, opts = {}) {
+		if (!comment?.cid || !comment?.id) {
+			throw new Error('comment param needs "cid" and "id" properties');
+		}
+		super(comment.id, client, opts);
+		this._payload = opts?.data ?? comment;
+		this.request_url = opts?.url ?? `/content/${comment.cid}/comments/${comment.id}`;
 	}
 
+	/**
+	 * Text of the comment
+	 * @type {Promise<string>}
+	 */
 	get text() {
 		return this.get("text");
 	}
 
+	/**
+	 * Id of the comment
+	 * @type {Promise<string>}
+	 */
 	get id() {
 		return this.get("id");
 	}
 
+	/**
+	 * Reply of the comment
+	 * @type {Promise<Reply|null>}
+	 */
 	get latest_reply() {
-		return this.get("last_reply");
+		// @ts-ignore
+		return (async () => {
+			let comm = await this.get("last_reply");
+			return comm ? await this.client.get_comment(comm) : null; // Should always return reply
+		})();
 	}
 
 	/**
@@ -55,7 +78,7 @@ export default class Comment extends FreshObject {
 	 */
 	get post() {
 		return (async () => {
-			let post = await this.get("post");
+			let post = await this.get("content");
 			return get_post_type(post, this.client);
 		})();
 	}
@@ -79,38 +102,126 @@ export default class Comment extends FreshObject {
 		})();
 	}
 
+	/**
+	 * Id of the post that the comment was posted on
+	 * @type {Promise<string>}
+	 */
 	get post_id() {
 		return this.get("cid");
 	}
 
+	/**
+	 * State of the comment (idk what this is)\
+	 * `normal`
+	 * @type {Promise<string>}
+	 */
 	get state() {
 		return this.get("state");
 	}
 
+	/**
+	 * Has the client smiled this comment?
+	 * @type {Promise<boolean>}
+	 */
 	get is_smiled() {
 		return this.get("is_smiled");
 	}
 
+	/**
+	 * Has the client unsmiled this comment?
+	 * @type {Promise<boolean>}
+	 */
 	get is_unsmiled() {
 		return this.get("is_unsmiled");
 	}
 
+	/**
+	 * Has the comment been edited?
+	 * @type {Promise<boolean>}
+	 */
 	get is_edited() {
 		return this.get("is_edited");
 	}
 
+	/**
+	 * The attachment of the comment, if any
+	 * @type {Promise<ImagePost|VideoPost|null>}
+	 */
 	get attachment() {
 		return (async () => {
 			let attch = await this.get("content");
-			let post = attch?.content?.[0] ?? null;
-			post = !post ? attch?.content_from_links?.[0] : post;
-			post = !post ? attch?.giphy?.[0] : post;
+			let post =
+				attch?.content?.[0] ??
+				attch?.content_from_links?.[0] ??
+				attch?.giphy?.[0];
+
 			if (!post) return null;
-			return this.client.get_post(post.id);
+			return get_post_type(post, this.client);
 		})();
 	}
 
-	// TODO Finnish getters for comment, add Reply.js
+	/**
+	 * Users mentioned in the comment
+	 * @type {Promise<User[]>}
+	 */
+	get mentions() {
+		return (async () => {
+			let { mention_user = null } = await this.get("attachments");
+			if (!mention_user) return null;
+			if (!Array.isArray(mention_user)) {
+				throw new Error(`mention_user returned non array: ${mention_user}`);
+			}
+			return mention_user.map(
+				(user) => new User(user.id, this.client, { data: user })
+			);
+		})();
+	}
+
+	/**
+	 * Object with smile, unsmile, and reply count
+	 * @type {Promise<Object}
+	 */
+	get nums() {
+		return this.get("nums", {});
+	}
+
+	/**
+	 * How many smiles the comment has
+	 * @type {Promise<number>}
+	 */
+	get smile_count() {
+		return (async () => {
+			return (await this.nums)?.smiles ?? 0;
+		})();
+	}
+
+	/**
+	 * How many unsmiles the comment has
+	 * @type {Promise<number>}
+	 */
+	get unsmile_count() {
+		return (async () => {
+			return (await this.nums)?.unsmiles ?? 0;
+		})();
+	}
+
+	/**
+	 * How many replies the comment has
+	 * @type {Promise<number>}
+	 */
+	get reply_count() {
+		return (async () => {
+			return (await this.nums)?.smiles ?? 0;
+		})();
+	}
+
+	/**
+	 * Content thumbnails for the attachments
+	 * @type {Promise<Object|null>}
+	 */
+	get content_thumbs() {
+		return this.get("content_thumbs");
+	}
 
 	// TODO Add reply generator function for commments
 }
